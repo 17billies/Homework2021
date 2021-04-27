a06f99cd6a753b73feeffcf44164752f1c649aae
17billies <78721618+17billies@users.noreply.github.com>
2021-04-27 15:47:44 +0800

    Update main.c

--- hw11119/main.c
+++ hw11119/main.c
@@ -4,26 +4,26 @@
 
 
 
-int main(void)
-{ int t1, b1, two1[MAX], i1=0, c=0; //¤Q¶i¦ì¼Æ
+int main(void){ 
+    int t1, b1, two1[MAX], i1=0, c=0; //åé€²ä½æ•¸
     int t2, b2, two2[MAX], i2=0;
     int carry=0 ,count=0;
     scanf("%d %d",&t1,&t2);
 
 
     while(t1>0){
-    b1=t1%2;
-    t1=t1/2;
-    two1[i1]=b1;
-    i1++;//i+1¬O¤G¶i¦ìªº¦ì¼Æ
-    }//¤Q¶i¦ìÂà´«¦¨¤G¶i¦ì(­Ëªº)
+        b1=t1%2;
+        t1=t1/2;
+        two1[i1]=b1;
+        i1++;//i+1æ˜¯äºŒé€²ä½çš„ä½æ•¸
+    }//åé€²ä½è½‰æ›æˆäºŒé€²ä½(å€’çš„)
     while(t2>0){
-    b2=t2%2;
-    t2=t2/2;
-    two2[i2]=b2;
-    i2++;
-    }//¤Q¶i¦ìÂà´«¦¨¤G¶i¦ì(­Ëªº)
-if (i1>=i2){
+        b2=t2%2;
+        t2=t2/2;
+        two2[i2]=b2;
+        i2++;
+    }//åé€²ä½è½‰æ›æˆäºŒé€²ä½(å€’çš„)
+    if (i1>=i2){
         int sum[i1+1];
         for(int n=0;n<=i1;n++){
             sum[n]=((two1[n]^two2[n])^carry);
@@ -32,40 +32,29 @@ if (i1>=i2){
                 count+=1;}
             else{
                 count+=0;
-            }}
-            for(int c=i1;c>0;c--){
+            }
+        }
+        for(int c=i1;c>0;c--){
         printf("%d", sum[c]);
-
+        }
+    printf("\n%d",count);   
     }
-    printf("\n%d",count);    }
     else{
-        int sum[i2+1];//«Å§i¥[ªkªº¦ì¼Æ
+        int sum[i2+1];//å®£å‘ŠåŠ æ³•çš„ä½æ•¸
         for(int n=0;n<=i2;n++){
             sum[n]=((two1[n]^two2[n])^carry);
             carry=(two1[n]&two2[n])|(two1[n]&carry)|(two2[n]&carry);
             if(carry==1){
-                count+=1;}
+                count+=1;
+            }
             else{
                 count+=0;
-            }}
-            for(int c=i2;c>0;c--){
+            }
+        }
+    for(int c=i2;c>0;c--){
         printf("%d", sum[c]);}
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
+    return 0;
 }
 
 
